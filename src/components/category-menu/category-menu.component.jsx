4b55d5fb4611053bb72989c562936c5f5c44427a
Saintkops <erwanchevrier6@gmail.com>
2024-08-03 15:39:23 +0200

    update: updating component

--- src/components/category-menu/category-menu.component.jsx
+++ src/components/category-menu/category-menu.component.jsx
@@ -1,36 +1,9 @@
-import './categories-styles.scss'
+import './categories-menu-styles.scss'
 import CategoryItem from "../category-item/category-item.component";
 
-const CategorieMenu = () => {
-    const categories = [
-        {
-            id: 1,
-            title: "hats",
-            imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-        },
-        {
-            id: 2,
-            title: "jackets",
-            imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-        },
-        {
-            id: 3,
-            title: "sneakers",
-            imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-        },
-        {
-            id: 4,
-            title: "womens",
-            imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-        },
-        {
-            id: 5,
-            title: "mens",
-            imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-        },
-    ];
+const CategorieMenu = ({ categories }) => {
     return (
-        <div className="categories-container">
+        <div className="categories-menu-container">
             {categories.map((category) => (
                 <CategoryItem key={category.id} category={category} />
             ))}
