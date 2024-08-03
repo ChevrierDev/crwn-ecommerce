1f144df9630a0b3a2ed39f313f97887ec375af4a
Saintkops <erwanchevrier6@gmail.com>
2024-08-03 15:27:00 +0200

    refactor: decompose components

--- src/App.js
+++ src/App.js
@@ -1,43 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import CategorieMenu from "./components/category-menu/category-menu.component";
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneackers",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    },
-  ];
-
-  return (
-    <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <CategorieMenu />;
 };
 
 export default App;
