1b6f0ce24255ee4cbae0b20ebbbcf6120aceaa44
Saintkops <erwanchevrier6@gmail.com>
2024-08-04 12:00:27 +0200

    feat: set up form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import {
     signInWithGooglePopup,
     createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
 
 const SignIn = () => {
     const logGoogleUser = async () => {
@@ -18,6 +19,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with google Popup
             </button>
+            <SignUpForm />
         </div>
     )
 }
